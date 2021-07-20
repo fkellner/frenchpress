c0daaa08b23cf567d496584a6793f7d7037f3687
Florian Kellner <mr.florian.kellner@web.de>
2021-07-20 11:49:50 +0200

    use seeder to copy example data to right directory

--- database/seeders/BlogEntrySeeder.php
+++ database/seeders/BlogEntrySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\BlogEntry;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class BlogEntrySeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class BlogEntrySeeder extends Seeder
      */
     public function run()
     {
-        BlogEntry::create([
+        $ex1 = BlogEntry::create([
           'title' => 'Hello Blogosphere!',
           'content' => 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht – ein geradezu unorthographisches Leben.
 
@@ -23,9 +24,17 @@ Eines Tages aber beschloß eine kleine Zeile Blindtext, ihr Name war Lorem Ipsum
 
 Als es die ersten Hügel des Kursivgebirges erklommen hatte, warf es einen letzten Blick zurück auf die Skyline seiner Heimatstadt Buchstabhausen, die Headline von Alphabetdorf und die Subline seiner eigenen Straße, der Zeilengasse. Wehmütig lief ihm eine rhetorische Frage über die Wange, dann setzte es seinen Weg fort. Unterwegs traf es eine Copy. Die Copy warnte das Blindtextchen, da, wo sie herkäme wäre sie zigmal umgeschrieben worden und alles, was von ihrem Ursprung noch übrig wäre, sei das Wort "und" und das Blindtextchen solle umkehren und wieder in sein eigenes, sicheres Land zurückkehren.',
           'publication_date' => new Carbon('2021-07-18 12:00:00'),
-          'header_image' => 'anshu-a-french-press-unsplash.jpg'
+          'header_image' => ''
         ]);
-        BlogEntry::create([
+        $img_path1 = 'headers/'.$ex1->id.'/header.jpg';
+        Storage::disk('public')->put(
+          $img_path1,
+          Storage::get('example_images/anshu-a-french-press-unsplash.jpg'),
+        );
+        $ex1->header_image = $img_path1;
+        $ex1->save();
+
+        $ex2 = BlogEntry::create([
           'title' => 'Die Wortberge haben schon was für sich.',
           'content' => 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht – ein geradezu unorthographisches Leben.
 
@@ -33,9 +42,17 @@ Eines Tages aber beschloß eine kleine Zeile Blindtext, ihr Name war Lorem Ipsum
 
 Als es die ersten Hügel des Kursivgebirges erklommen hatte, warf es einen letzten Blick zurück auf die Skyline seiner Heimatstadt Buchstabhausen, die Headline von Alphabetdorf und die Subline seiner eigenen Straße, der Zeilengasse. Wehmütig lief ihm eine rhetorische Frage über die Wange, dann setzte es seinen Weg fort. Unterwegs traf es eine Copy. Die Copy warnte das Blindtextchen, da, wo sie herkäme wäre sie zigmal umgeschrieben worden und alles, was von ihrem Ursprung noch übrig wäre, sei das Wort "und" und das Blindtextchen solle umkehren und wieder in sein eigenes, sicheres Land zurückkehren.',
           'publication_date' => new Carbon('2021-06-18 10:00:00'),
-          'header_image' => 'daffa-z-french-press-unsplash.jpg'
+          'header_image' => ''
         ]);
-        BlogEntry::create([
+        $img_path2 = 'headers/'.$ex2->id.'/header.jpg';
+        Storage::disk('public')->put(
+          $img_path2,
+          Storage::get('example_images/daffa-z-french-press-unsplash.jpg'),
+        );
+        $ex2->header_image = $img_path2;
+        $ex2->save();
+
+        $ex3 = BlogEntry::create([
           'title' => 'Bazinga!',
           'content' => 'Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans. Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht – ein geradezu unorthographisches Leben.
 
@@ -43,7 +60,15 @@ Eines Tages aber beschloß eine kleine Zeile Blindtext, ihr Name war Lorem Ipsum
 
 Als es die ersten Hügel des Kursivgebirges erklommen hatte, warf es einen letzten Blick zurück auf die Skyline seiner Heimatstadt Buchstabhausen, die Headline von Alphabetdorf und die Subline seiner eigenen Straße, der Zeilengasse. Wehmütig lief ihm eine rhetorische Frage über die Wange, dann setzte es seinen Weg fort. Unterwegs traf es eine Copy. Die Copy warnte das Blindtextchen, da, wo sie herkäme wäre sie zigmal umgeschrieben worden und alles, was von ihrem Ursprung noch übrig wäre, sei das Wort "und" und das Blindtextchen solle umkehren und wieder in sein eigenes, sicheres Land zurückkehren.',
           'publication_date' => new Carbon('2021-08-18 14:00:00'),
-          'header_image' => 'rachel-brenner-french-press-unsplash.jpg'
+          'header_image' => ''
         ]);
+        $img_path3 = 'headers/'.$ex3->id.'/header.jpg';
+        Storage::disk('public')->put(
+          $img_path3,
+          Storage::get('example_images/rachel-brenner-french-press-unsplash.jpg'),
+        );
+        $ex3->header_image = $img_path3;
+        $ex3->save();
+
     }
 }
