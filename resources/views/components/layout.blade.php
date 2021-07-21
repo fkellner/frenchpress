92d5a467df9768701b3fd89e8a515da46dd742ec
Florian Kellner <mr.florian.kellner@web.de>
2021-07-21 13:22:13 +0200

    fix typo including script

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'MyFunkyBlog' }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
     <script>
 document.addEventListener('DOMContentLoaded', () => {
 
